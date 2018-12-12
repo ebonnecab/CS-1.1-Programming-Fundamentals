149c3e6bdeecb8924f298fd4a9631ee8c6f037fc
William Bogans <williambogans98@gmail.com>
2018-12-12 06:49:03 -0800

    finished the run

--- simulation.py
+++ simulation.py
@@ -59,35 +59,20 @@ class Simulation(object):
         pass
 
     def _simulation_should_continue(self):
-        ''' The simulation should only end if the entire population is dead
-        or everyone is vaccinated.
-
-            Returns:
-                bool: True for simulation should continue, False if it should end.
-        '''
-        # TODO: Complete this helper method.  Returns a Boolean.
-        pass
+        return len(self.population) == 0 or self.vacc_percentage == 1
 
     def run(self):
         ''' This method should run the simulation until all requirements for ending
         the simulation are met.
         '''
-        # TODO: Finish this method.  To simplify the logic here, use the helper method
-        # _simulation_should_continue() to tell us whether or not we should continue
-        # the simulation and run at least 1 more time_step.
-
-        # TODO: Keep track of the number of time steps that have passed.
-        # HINT: You may want to call the logger's log_time_step() method at the end of each time step.
-        # TODO: Set this variable using a helper
         time_step_counter = 0
-        should_continue = None
-
+        should_continue = self._simulation_should_continue()
         while should_continue:
-            # TODO: for every iteration of this loop, call self.time_step() to compute another
-            # round of this simulation.
-            print('The simulation has ended after {time_step_counter} turns.'.format(
-                time_step_counter))
-        pass
+            self.time_step()
+            self.logger.log_time_step(time_step_counter)
+            time_step_counter += 1
+        print('The simulation has ended after {time_step_counter} turns.'.format(
+            time_step_counter))
 
     def time_step(self):
 
